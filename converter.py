36ce83b33d25337f158af075d7c81628ddad0fdc
PJAntonio <pjantonionj@gmail.com>
2025-10-03 20:58:15 -0400

    added bytes object acceptance, API now accepts image and returns the converted image via bytes without saving to disk

--- converter.py
+++ converter.py
@@ -1,28 +1,13 @@
 from PIL import Image
-from pathlib import Path
 from pillow_heif import register_heif_opener
 import io
 
-register_heif_opener()
 
-img_suffix = {
-    'jpeg': '.jpeg',
-    'png': '.png'
-}
+register_heif_opener() #Enables HEIC files manipulation
 
 
-def heic_converter(source_path, format):
-    path = Path(source_path)
-    img = Image.open(path)
-    buffer = io.BytesIO()   
-    img.save(buffer, format, optimize = True, quality = 40)
-    img_b = buffer.getvalue()
-    
-
-
-format = 'png'
-img_path = 'IMG_0901.HEIC'
-
-heic_converter(img_path, format)
-
-
+def heic_converter(bytes_file: bytes, format: str) -> bytes: 
+    img = Image.open(io.BytesIO(bytes_file)) #Open the received image as bytes
+    buffer = io.BytesIO() #Create bytes buffer for the conversion
+    img.save(buffer, format, optimize = True, quality = 40)  
+    return buffer.getvalue() #Returns the bytes from the converted image
